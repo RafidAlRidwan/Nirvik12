a5913091d1ff6a5a9e32eac60b49789ff64d0ffe
Rafid Al Ridwan <Ridwan@tvista-bd.com>
2022-04-21 10:34:54 +0600

    Upp

--- resources/views/user/user-login/password_reset.blade.php
+++ resources/views/user/user-login/password_reset.blade.php
@@ -2,19 +2,18 @@
 @section('style')
 @endsection
 @section('content')
+@php
+$cache = Cache::get('settings');
+$banner = $cache->where('key', 'banner')->first();
+@endphp
 <div class="login-content">
   <div class="form-box">
     <form method="POST" class="box" action="{{ route('password.email') }}">
       @csrf
       <img src="{{asset('assets/user/loginPage/img/bzs.png')}}">
-      <h3 class="title">নির্ভীক '১২</h3>
+      <a href={{route('landingPage')}}><img src="{{asset($banner->value)}}" alt="নির্ভীক'১২"></a>
 
       <input type="email" id="email" name="email" placeholder="E-Mail Address" autocomplete="off" value="{{ old('email') }}" class="input @error('email') is-invalid @enderror" required autocomplete="off">
-
-
-      <div style="justify-content: center">
-        <button type="submit" class="btn">{{ __('Send Link') }}</button>
-      </div>
       @if (session('status'))
       <div style="color: green;" class="alert alert-success" role="alert">
         <strong>{{ session('status') }}</strong>
@@ -26,8 +25,13 @@
       </span>
       @enderror
 
-
+      <div style="justify-content: center">
+        <button type="submit" class="btn">{{ __('Send Link') }}</button>
+      </div>
     </form>
+    <div style="justify-content: center">
+      <a href={{route('user_login')}}><button class="btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i></button></a>
+    </div>
   </div>
 </div>
 @endsection
